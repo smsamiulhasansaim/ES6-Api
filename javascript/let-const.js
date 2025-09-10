45d80c425e9c5c505f937f22b5f12dee39ddd4d2
smsamiulhasansaim <hello.smsamiulhasan@gmail.com>
2025-09-10 06:08:34 +0600

    let-const

---
+++ javascript/let-const.js
@@ -0,0 +1,17 @@
+// es6
+const name = 'Bangladesh';
+const countryName = 'BD' + name;
+
+console.log(countryName)
+
+// <---------------------------->
+const dress = ['shirt', 'pant', 'jacket'];
+dress.push('Apex Juta')
+
+console.log(dress)
+
+// <---------------------------->
+const student = {namme: 'jaglu', marks: 40}
+student.marks = 10;
+
+console.log(student)
